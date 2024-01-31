fefb464177942f58842ca9c2ae9eaa0c65cf332d
404pandas <113853550+404pandas@users.noreply.github.com>
2024-01-31 14:29:08 -0500

    Relations fixed, seeds working

--- models/index.js
+++ models/index.js
@@ -3,31 +3,14 @@ const Letter = require('./Letter');
 const Shape = require('./Shape');
 const Number = require('./Number');
 
-User.hasMany(Letter, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
+User.hasMany(Letter, { as: 'savedLetters' });
+User.hasMany(Shape, { as: 'savedShapes' });
+User.hasMany(Number, { as: 'savedItems' });
 
-User.hasMany(Shape, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
+Letter.belongsTo(User);
 
-User.hasMany(Number, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE',
-});
+Shape.belongsTo(User);
 
-Letter.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
-Shape.belongsTo(User, {
-  foreignKey: 'user_id',
-});
-
-Number.belongsTo(User, {
-  foreignKey: 'user_id',
-});
+Number.belongsTo(User);
 
 module.exports = { User, Letter, Shape, Number };
