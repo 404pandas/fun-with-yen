d8e7ed70fcb7229eae601d7f3b26c825ce21afb5
404pandas <113853550+404pandas@users.noreply.github.com>
2024-02-06 10:19:51 -0500

    Items displaying on 'canvas'

---
+++ public/js/home.js
@@ -0,0 +1,23 @@
+function displaySelectedItemDetails(name, file_path) {
+  // Get a reference to the selectedItemDetails div
+  const selectedItemDetailsDiv = document.getElementById('selectedItemDetails');
+
+  // Create HTML content with the selected item's details
+  const detailsHTML = `
+      <h5>Let's trace: ${name}!</h5>
+      <img src=${file_path} id="current-trace"/>
+    `;
+
+  // Update the content of the div
+  selectedItemDetailsDiv.innerHTML = detailsHTML;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.dropdown-trigger');
+  var instances = M.Dropdown.init(elems);
+});
